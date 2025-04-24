bfd60381c245782a44632da86e0a027334bb0ced
David Kozma <kozmadavid90@gmail.com>
2025-04-24 13:30:29 +0200

    Setting up the project
    Using raylib game premake

---
+++ game/src/main.cpp
@@ -0,0 +1,20 @@
+#include "raylib.h"
+
+int main()
+{
+    InitWindow(800, 600, "Raylib Example");
+	SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawText("Hello Raylib!", 10, 10, 20, BLACK);
+
+		EndDrawing();
+    }
+    CloseWindow();
+
+    return 0;
+}
