5f212fd9fe528845b7913e421042280e69733f69
David Kozma <kozmadavid90@gmail.com>
2025-07-27 18:38:48 +0200

    Refactoring Card class
    Renderer
    Card
    CardCoordinates

--- game/src/Card.cpp
+++ game/src/Card.cpp
@@ -2,21 +2,14 @@
 
 Solitaire::Card::Card(Rank rank, Suit suit) : m_Rank(rank), m_Suit(suit) 
 {
-    const int cardOffsetX = static_cast<int>(m_Rank) - 1;
-    const int cardOffsetY = static_cast<int>(m_Suit);
+    const IVector2 offset = { static_cast<int>(m_Rank) - 1, static_cast<int>(m_Suit) };
 
-    m_SourceRectangle = 
-    {
-        static_cast<float>(Constants::ORIGIN_X + (cardOffsetX * Constants::SPRITE_OFFSET_X)), 
-        static_cast<float>(Constants::ORIGIN_Y + (cardOffsetY * Constants::SPRITE_OFFSET_Y)), 
-        static_cast<float>(Constants::SPRITE_WIDTH), 
-        static_cast<float>(Constants::SPRITE_HEIGHT)
-    };
+    m_Coordinates.SetCardSourceCoordinates(offset);
 }
 
-Solitaire::Card::Card(Rank rank, Suit suit, Vector2 offset) : Card(rank, suit)
+Solitaire::Card::Card(Rank rank, Suit suit, IVector2 offset) : Card(rank, suit)
 {
-    this->SetCoords(offset);
+    m_Coordinates.SetCardDestinationCoordinates(offset);
 }
 
 Solitaire::Rank Solitaire::Card::GetRank() const 
@@ -29,34 +22,7 @@ Solitaire::Suit Solitaire::Card::GetSuit() const
     return m_Suit; 
 }
 
-void Solitaire::Card::Render(Texture2D& texture)
-{        
-    DrawTexturePro(texture, m_SourceRectangle, m_DestinationRectangle, {0, 0}, 0.0f, WHITE);
-}
-
-Rectangle Solitaire::Card::GetCoords()
-{ 
-    return m_DestinationRectangle; 
-}
-
-void Solitaire::Card::PrintCardCoordinates() const
+Solitaire::CardCoordinates& Solitaire::Card::GetCoords()
 {
-    std::cout << "Card (x0, y0, width, height): " << m_DestinationRectangle.x << ", " << m_DestinationRectangle.y << ", " 
-              << m_DestinationRectangle.width << ", " << m_DestinationRectangle.height << std::endl;
-}
-
-void Solitaire::Card::SetCardCoordX(int x)
-{
-    m_DestinationRectangle.x = x;
-}
-
-void Solitaire::Card::SetCardCoordY(int y)
-{
-    m_DestinationRectangle.y = y;
-}
-
-void Solitaire::Card::SetCoords(Vector2 offset)
-{
-    m_DestinationRectangle.x = offset.x;
-    m_DestinationRectangle.y = offset.y;
+    return m_Coordinates;
 }
--- game/src/Card.h
+++ game/src/Card.h
@@ -3,6 +3,8 @@
 #include <raylib.h>
 #include <iostream>
 #include "Constants.h"
+#include "CardCoordinates.h"
+#include "Utils.h"
 
 
 namespace Solitaire
@@ -16,22 +18,16 @@ namespace Solitaire
         Card() = delete; // TODO: for now...
         ~Card() = default;
         Card(Rank rank, Suit suit);
-        Card(Rank rank, Suit suit, Vector2 offset);
+        Card(Rank rank, Suit suit, IVector2 offset);
 
         Rank GetRank() const;
         Suit GetSuit() const;
 
-        void Render(Texture2D& texture);
-        Rectangle GetCoords();
-        void PrintCardCoordinates() const;
-        void SetCardCoordX(int x);
-        void SetCardCoordY(int y);
-        void SetCoords(Vector2 offset);
+        CardCoordinates& GetCoords();
 
     private:
         Rank m_Rank = Rank::Ace;
         Suit m_Suit = Suit::Hearts;
-        Rectangle m_DestinationRectangle = { 0, 0, Constants::RENDERED_SPRITE_WIDTH, Constants::RENDERED_SPRITE_HEIGHT };
-        Rectangle m_SourceRectangle = {0, 0, 0, 0};
+        CardCoordinates m_Coordinates;
     };
 }
